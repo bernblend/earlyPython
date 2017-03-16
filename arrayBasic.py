16a8d71c06fc2fdd1c0e07c2c5e37d721e1edb94
bernblend <bernblend@gmail.com>
2017-03-16 14:52:07 -0700

    Uploaded arrayBasic.py

---
+++ arrayBasic.py
@@ -0,0 +1,17 @@
+name = 'Hector'
+print 'Hello ' + name + '!' * 3
+
+print name[-1]
+
+
+s = "some string"
+
+print s[3],s[1+1+1]
+
+print s[0],(s+s)[0]
+
+print s[0] + s[1],s[0 + 1]
+
+print s[1],(s + 'ity')[1]  #trick question - NOT same value all the time.
+
+print s[-1],(s + s)[-1]
