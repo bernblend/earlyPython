a456ced9dc471a9cd8aad19c1d06acd695ca11e9
bernblend <bernblend@gmail.com>
2017-03-16 14:05:23 -0700

    Uploaded nonstick.py & ageInDays.py

---
+++ ageInDays.py
@@ -0,0 +1,5 @@
+# my age in days
+age = 31
+days_in_year = 365
+days_since_bday = 198
+print age * days_in_year + days_since_bday
---
+++ nanostick.py
@@ -0,0 +1,23 @@
+speed_of_light = 299792458   # meters per second
+
+
+cycles_per_second = 2700000000.  # 2.7 GHz  Note: decimal for division
+
+cycle_distance_in_meters = speed_of_light / cycles_per_second
+
+cycle_distance_in_centimeters = cycle_distance_in_meters * 100
+
+print cycle_distance_in_meters
+
+print cycle_distance_in_centimeters
+
+
+cycles_per_second = 2800000000.   # 2.8 GHz
+
+cycle_distance_in_meters = speed_of_light / cycles_per_second
+
+cycle_distance_in_centimeters = cycle_distance_in_meters * 100
+
+print cycle_distance_in_meters
+
+print cycle_distance_in_centimeters
