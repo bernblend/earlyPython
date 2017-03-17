5f0cc4e2398cdddd228d727e0bf3af111d4709f0
bernblend <bernblend@gmail.com>
2017-03-17 09:58:58 -0700

    Uploaded strings.py

---
+++ strings.py
@@ -0,0 +1,27 @@
+
+print 7 * 7 * 24   # number of days in a week
+
+print "***************************"
+
+# Given the variables s and t defined as:
+s = 'udacity'
+t = 'bodacious'
+# write Python code that prints out udacious
+# without using any quote characters in
+# your code.
+
+
+print s[0] + t[2:]
+
+print "***************************"
+
+
+# Insert the proper slicing indices for the substring variable
+# so that the slice is a string that contains everything after "A NOUN".
+# For example, if we wanted to store everything after "went", the returned
+# string would be equal to sentence[11:].
+
+sentence = "A NOUN went on a walk."
+substring = sentence[6:]
+
+print substring
