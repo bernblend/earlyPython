884dfe63869ce7b3fb2185608f3549b3df9ce293
bernblend <bernblend@gmail.com>
2017-03-16 23:13:54 -0700

    Added examples for .find( ) - 2nd parameter

--- findMethod.py
+++ findMethod.py
@@ -21,7 +21,7 @@ print s.find('')    # always 0, space is thher
 print s.find(s + '!!!') + 1   # always 0, because 1st part is always -1, "!!!" is not found in the string
 
 
-# This segment is just a chance for you to play around with 
+# This segment is just a chance for you to play around with
 # finding strings within strings. Read through the code and
 # press Test Run to see what it does. Is there anything
 # interesting or unexpected?
@@ -40,12 +40,29 @@ print my_string[2:]
 print "Example 3: Printing out everything after a certain substring"
 my_string = "My favorite color: blue"
 color_start_location = my_string.find("color:")
+print color_start_location
 favorite_color = my_string[color_start_location:]
 print favorite_color # oops, this line prints out 'color: ' as well...
-print favorite_color[7:] # this fixes it!
+print favorite_color[7:] # this fixes it! favorite_color has new index
+
 
 print "Example 4: Other interesting things about string.find()..."
 print "text".find("text") # prints 0
 print "text".find("Text") # prints -1
 print "text".find("")     # prints 0
 print "text".find(" ")    # prints -1
+
+
+print '********** MORE of .find() ***************'
+
+print "text".find("t", 1)  # starts from index 1 to find letter
+
+
+
+print '********** more on 2nd parameter ***************'
+
+danton = "De l'audace, encore de l'audace, toujours de l'audace"
+print danton.find('audace')
+print danton.find('audace', 0)   # same, as it starts at index 0
+print danton.find('audace', 5)  # still finds first audace at index 5
+print danton.find('audace', 6)  # now goes to next audace in string
