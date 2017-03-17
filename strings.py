0ef08493dfed5598ad6fe409e8715e0212ec716f
bernblend <bernblend@gmail.com>
2017-03-17 10:05:19 -0700

    Added examples to strings.py

--- strings.py
+++ strings.py
@@ -13,6 +13,8 @@ t = 'bodacious'
 
 print s[0] + t[2:]
 
+
+
 print "***************************"
 
 
@@ -25,3 +27,21 @@ sentence = "A NOUN went on a walk."
 substring = sentence[6:]
 
 print substring
+
+
+
+
+print "***************************"
+
+# Use string slicing to store everything before "NOUN" in substring1,
+# everything after "NOUN" and before "VERB" in substring2, and everything after "VERB"
+# in substring3.
+
+sentence = "A NOUN went on a walk. It can VERB really fast."
+substring1 = sentence[:2]
+substring2 = sentence[6:30]
+substring3 = sentence[34:]
+
+print substring1
+print substring2
+print substring3
