dba6dec38a270ebc4e8949fab5c7a1322716804d
bernblend <bernblend@gmail.com>
2017-03-29 23:14:55 -0700

    Added infiniteLoop.py, removeSpaces.py

---
+++ infiniteLoop.py
@@ -0,0 +1,9 @@
+# This loop will go on forever, or until the computer runs out of memory.
+
+
+print "\nWhat will this program do?\n"
+
+i = 1
+while i != 10:
+    i = i + 2
+    print i
---
+++ whileLoop.py
@@ -0,0 +1,20 @@
+
+# First example of while loop.
+
+i = 0
+
+while i < 10:
+    print i
+    i = i + 1
+
+
+
+print "\n********************\n\n"
+print "   Another Example    \n"
+
+
+i = 0
+
+while i != 10:
+    i = i + 1
+    print i
